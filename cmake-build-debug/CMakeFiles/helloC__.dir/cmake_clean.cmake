72630ab6291ac35979914daf9c2d9f5f0576941a
Gilbert <gssenyonjo@gmail.com>
2020-01-22 19:28:52 +0300

    Created Hello world

---
+++ cmake-build-debug/CMakeFiles/helloC__.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloC__.dir/main.cpp.obj"
+  "helloC__.exe"
+  "helloC__.exe.manifest"
+  "helloC__.lib"
+  "helloC__.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloC__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
