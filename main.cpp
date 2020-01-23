f0d2ff26ec88cdb78ccf2a657d78d18a224b2481
Gilbert <gssenyonjo@gmail.com>
2020-01-23 12:21:25 +0300

    finished

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
-#include <iostream>
+// Simple C++ Program
+# include <iostream>
+# include <string>
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // ask for a person's name
+    std::cout << "Whats your name";
+
+    // read the name
+    std::string name;
+    std::cin >> name;
+
+    // write  name
+    std::cout << name << std::endl;
+
+
     return 0;
 }
